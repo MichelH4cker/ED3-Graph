57a38b36bf0f21f552a4a2291f108191473bf773
MichelH4cker <michel.hecker@usp.br>
2022-12-12 01:18:26 -0300

    add graph class with its methods

--- src/graph.h
+++ src/graph.h
@@ -18,4 +18,26 @@ typedef struct vertex {
 } vertex;
 
 
+/**
+ * @brief 
+ * 
+ */
+class Graph {
+    public: 
+        map<int, vertex> vertices;
+        map<int, vertex>::iterator it;        
+        forward_list<edge>::iterator itl;
+
+        int numberOfVertices;
+
+        void createGraph(char *db_file);
+        void sortList();
+        void printVertex(vertex vertex);
+        vertex createVertex(const register_db &reg_db);
+        edge createEdge(const register_db &reg_db);
+        vertex updateVertexToIdPoPs(register_db reg_db);
+        edge updateEdgeToIdPoPs(register_db reg_db);
+        void printGraph();
+};
+
 #endif
