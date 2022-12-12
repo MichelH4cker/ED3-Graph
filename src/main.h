406010bc6300eb742c6a5a9ace611eeb392609dc
MichelH4cker <michel.hecker@usp.br>
2022-12-12 01:28:10 -0300

    add new includes

--- src/main.h
+++ src/main.h
@@ -6,8 +6,13 @@
 #include <iostream>
 #include <string.h>
 #include <list>
+#include <map>
+#include <forward_list>
 #include <iterator>
 
 using namespace std;
+
+#include "db_file.h"
+#include "graph.h"
 
 #endif
