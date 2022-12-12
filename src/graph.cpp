c33e32f50a3dce564a4f0185625986854c538f6b
MichelH4cker <michel.hecker@usp.br>
2022-12-12 01:24:00 -0300

    create a function to create a vertex

--- src/graph.cpp
+++ src/graph.cpp
@@ -14,3 +14,14 @@ void Graph::printVertex(vertex vertex){
     cout << vertex.idConecta << " | " << vertex.nomePais << " | " << vertex.nomePoPs << " | " << vertex.siglaPais << endl;
 }
 
+vertex Graph::createVertex(const register_db &reg_db){
+    vertex vertex;
+    vertex.idConecta = reg_db.idConecta;
+
+    vertex.nomePais = reg_db.nomePais;
+    vertex.nomePoPs = reg_db.nomePoPs;
+    vertex.siglaPais = reg_db.siglaPais;
+    
+    return vertex;
+}
+
