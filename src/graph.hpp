476c09c72a2925bb7e86e704e092d881af652108
MichelH4cker <michel.hecker@usp.br>
2022-12-12 01:35:16 -0300

    add documentary sketch

--- src/graph.hpp
+++ src/graph.hpp
@@ -17,6 +17,14 @@ typedef struct vertex {
     forward_list<edge> edges;
 } vertex;
 
+/**
+ * @brief 
+ * 
+ * @param x 
+ * @param y 
+ * @return true 
+ * @return false 
+ */
 bool my_compare (const edge x,const edge y);
 
 /**
@@ -31,13 +39,62 @@ class Graph {
 
         int numberOfVertices;
 
+        /**
+         * @brief Create a Graph object
+         * 
+         * @param db_file 
+         */
         void createGraph(char *db_file);
+
+        /**
+         * @brief 
+         * 
+         */
         void sortList();
+
+        /**
+         * @brief 
+         * 
+         * @param vertex 
+         */
         void printVertex(vertex vertex);
+        
+        /**
+         * @brief Create a Vertex object
+         * 
+         * @param reg_db 
+         * @return vertex 
+         */
         vertex createVertex(const register_db &reg_db);
+      
+        /**
+         * @brief Create a Edge object
+         * 
+         * @param reg_db 
+         * @return edge 
+         */
         edge createEdge(const register_db &reg_db);
+      
+        /**
+         * @brief 
+         * 
+         * @param reg_db 
+         * @return vertex 
+         */
         vertex updateVertexToIdPoPs(register_db reg_db);
+        
+        /**
+         * @brief 
+         * 
+         * @param reg_db 
+         * @return edge 
+         */
         edge updateEdgeToIdPoPs(register_db reg_db);
+      
+        /**
+         * @brief 
+         * 
+         */
         void printGraph();
 };
 
