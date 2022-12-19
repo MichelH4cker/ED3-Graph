5859abc1220ff5cc331e3f9b045ed02df42b72a5
MichelH4cker <michel.hecker@usp.br>
2022-12-18 21:54:52 -0300

    style: remove unnecesarry function initialize_default

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,5 @@
 #include "main.hpp"
 
-Graph inicialize_default(char *db_file) {
-    Graph graph;
-    graph.createGraph(db_file);
-    graph.sortList();
-    return graph;
-}
-
 void menu() {
     int command;
     scanf("%d\n", &command);
@@ -15,24 +8,23 @@ void menu() {
     scanf("%s", db_file);
 
     Graph graph;
+    graph.createGraph(db_file);
+    graph.sortList();
+    
     int orig;
     int dest;
 
     switch (command) {
         case 11:
-            graph = inicialize_default(db_file);
             graph.printGraph();
             break;
         case 12:
-            graph = inicialize_default(db_file);
             graph.count_cycles();
             break;
         case 13:
-            graph = inicialize_default(db_file);
             graph.flow_max();
             break;
         case 14:
-            graph = inicialize_default(db_file);
             graph.shortestPathWithStop();
             break;
         default:
